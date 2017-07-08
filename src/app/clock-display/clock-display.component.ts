4b43d6a85a0eba359a92a0a2bbd95dc633c2b429
Tim G <tmgllghr@gmail.com>
2017-07-08 15:30:16 -0400

    Initial Commit

---
+++ src/app/clock-display/clock-display.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clock-display',
+  templateUrl: './clock-display.component.html',
+  styleUrls: ['./clock-display.component.css']
+})
+export class ClockDisplayComponent implements OnInit {
+  
+   timer: number;  
+   time: number;
+
+  constructor() {
+    this.time = Date.now();
+    this.initializeTimer();
+    this.runTimer();
+  }
+
+  initializeTimer (){
+    this.timer = 20;
+  }
+
+  runTimer(){
+    setInterval(()=>{ this.timer -= 1 },1000);
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+
+/*
+ TODO: create timer object that holds the 2 values in mins and secs
+ and is reposnible for it's own display.
+
+ TODO: set interval on the seconds once it hits zero 
+ fire an event that will reduce the minutes
+ avoid nested loops - tyr observable here 
+
+TODO:Move timer logic to a lower lever 
+the display tag will be responsible for the display logic. 
+The timer will push up the time value
+
+TODO: create controls to increment timer up and down  from 
+default start value. 
+
+TODO: create the timer as a service
+pros cons of sep component vs. service
+
+TODO: imput and display for what your are working on 
+
+TODO: add storage for what you're working on and display all
+
+*/
